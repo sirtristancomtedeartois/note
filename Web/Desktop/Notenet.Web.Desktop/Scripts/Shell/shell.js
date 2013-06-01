e82e0bed3fdcfe07f0e692fcbda85e7874927bc6
sirtristancomtedeartois <comtedeartois@hotmail.com>
2013-06-01 14:00:34 +0800

    Update shell.js

--- Web/Desktop/Notenet.Web.Desktop/Scripts/Shell/shell.js
+++ Web/Desktop/Notenet.Web.Desktop/Scripts/Shell/shell.js
@@ -1,2 +1,49 @@
-﻿(function ($) {
+(function ($) {
+    /*app register*/
+    var apps = [];
+
+    $(this).bind("registerApp", function (event, app) {
+        if (app.appMeta.type = "window") {
+            if (apps[app.appMeta.name] === undefined) {
+                apps[app.appMeta.name] = app;
+            }
+        }
+    });
+
+    /*app ui cache*/
+    var appUICache = [];
+
+    var addToAppUICache = function (app) {
+        $("windowAppCache").prepend("<div></div>").prepend(app.html);
+        appUICache[app.appMeta.name] = $("windowAppCache:first-child");
+        appUICache[app.appMeta.name].css("display", "none");
+        appUICache[app.appMeta.name].addClass("windowApp")
+    }
+
+    $(this).bind("cacheAppUI", function (event, app) {
+        if (app.appMeta.type === "window") {
+            addToAppUICache(app);
+        }
+    });
+
+    /*open an app*/
+    $(this).bind("open", function (event, appMeta, header, para) {
+        if (appMeta.type = "window") {
+            var appUIName = appMeta.name;
+            if (header !== undefined) {
+                appUIName = appUIName + "." + header;
+            }
+
+            if (appUICache[appUIName] === undefined) {
+                if (apps[appMeta.name] !== undefiend) {
+                    var app = { "appMeta": appMeta, "html": apps[appMeta.name].main(header, para) };
+                    appMeta.name = appUIName;
+                }
+                addToAppUICache(app);
+            }
+
+            appUICache[appUIName].show();
+            appUICache["windowAppCache"].show("slide", {}, 500, callback)
+        }
+    });
 })(jQuery);
